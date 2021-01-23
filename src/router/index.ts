384d81f6edb98822fa4165f5ede8efbdff53eae6
Raj Kishore <40551183+RKBL4ZE@users.noreply.github.com>
2021-01-23 17:44:21 +0530

    ADD: header

---
+++ src/router/index.ts
@@ -0,0 +1,55 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import CoachDetail from '../views/coaches/CoachDetail.vue'
+import CoachesList from '../views/coaches/CoachesList.vue'
+import CoachRegistration from '../views/coaches/CoachRegistration.vue'
+import ContactCoach from '../views/requests/ContactCoach.vue'
+import RequestReceived from '../views/requests/RequestReceived.vue'
+import NotFound from '../views/NotFound.vue'
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/coaches'
+  },
+  {
+    path: '/coaches',
+    component: CoachesList
+  },
+  {
+    path: '/coaches/:id',
+    component: CoachDetail,
+    children: [
+      {
+        path: 'contact',
+        component: ContactCoach
+      }
+    ]
+  },
+  {
+    path: '/register',
+    component: CoachRegistration
+  },
+  {
+    path: '/requests',
+    component: RequestReceived
+  },
+  {
+    path: '/:notFound(.*)',
+    component: NotFound
+  }
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
